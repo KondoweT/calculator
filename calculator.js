b880d439b7b132ecf7888c266016a2f13361e5f3
KondoweT <mukukakondowe@gmail.com>
2022-05-20 17:16:18 +0200

    create operator function

--- calculator.js
+++ calculator.js
@@ -5,3 +5,25 @@ function add(a,b){
 function subtract(a,b){
     return a -b;
 }
+
+function multipy(a,b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function operator(a,b,ope){
+    if(ope === '/'){
+        return divide(a,b);
+    }else if(ope === '+'){
+        return sum(a,b);
+    }else if(ope === '-'){
+        return subtract(a,b)
+    }else if(ope === '*'){
+        return multipy(a,b);
+    }else{
+        return 'MATH ERROR';
+    }
+}
