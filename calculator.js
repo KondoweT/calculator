34a3096e3c5977a8ac1a09ec6ef6836689abd870
KondoweT <mukukakondowe@gmail.com>
2022-06-02 08:55:15 +0200

    ensure that numbers are put correctly

--- calculator.js
+++ calculator.js
@@ -1,3 +1,30 @@
+// functions for the visual appearance of the calculator button
+
+const changeButtonSize = document.querySelectorAll('.size');
+changeButtonSize.forEach(item=>{
+    item.addEventListener('mousedown', function onClick() {
+        item.style.borderRadius = '25px';
+      })
+});
+
+changeButtonSize.forEach(item=>{
+    item.addEventListener('mouseup', function onClick() {
+        item.style.borderRadius = '50%';
+      })
+});
+
+const equal = document.querySelector('.equal');
+equal.addEventListener('mousedown', function color() {
+    equal.style.backgroundColor = '#42b1b1';
+})
+
+equal.addEventListener('mouseup', function color() {
+    equal.style.backgroundColor = '#d3e3fd';
+})
+
+
+// functions the working of the calculator below
+
 function add(a,b){
     return a + b;
 }
@@ -36,24 +63,26 @@ function operator(a,b,ope){
     }
 }
 
-const changeButtonSize = document.querySelectorAll('.size');
-changeButtonSize.forEach(item=>{
-    item.addEventListener('mousedown', function onClick() {
-        item.style.borderRadius = '25px';
-      })
+const numbers = document.querySelectorAll('.num');
+const operations = document.querySelector('.operations');
+const numb = document.createElement('div');
+numb.classList.add('bigFont');
+
+let op1 = '';
+numbers.forEach(num =>{
+    num.addEventListener('click', function(){
+        let text = num.id;
+        if(text === '.'){
+            let result = op1.indexOf('.');
+            if(result < 0){
+                op1 += text;    
+            }
+        }else{
+            op1 += text;
+        }
+        numb.textContent = op1;
+        operations.appendChild(numb)
+        console.log(op1);
+    })
 });
 
-changeButtonSize.forEach(item=>{
-    item.addEventListener('mouseup', function onClick() {
-        item.style.borderRadius = '50%';
-      })
-});
-
-const equal = document.querySelector('.equal');
-equal.addEventListener('mousedown', function color() {
-    equal.style.backgroundColor = '#42b1b1';
-})
-
-equal.addEventListener('mouseup', function color() {
-    equal.style.backgroundColor = '#d3e3fd';
-})
