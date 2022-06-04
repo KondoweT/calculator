63bc54c3c82e99833de2a0870310f60ff466a776
KondoweT <mukukakondowe@gmail.com>
2022-06-04 19:28:27 +0200

    made a calculator work correctly

--- calculator.js
+++ calculator.js
@@ -9,7 +9,7 @@ changeButtonSize.forEach(item=>{
 
 changeButtonSize.forEach(item=>{
     item.addEventListener('mouseup', function onClick() {
-        item.style.borderRadius = '50%';
+        item.style.borderRadius = '10%';
       })
 });
 
@@ -34,21 +34,34 @@ let subtract = function (a,b){
 }
 
 let multipy = function (a,b){
-    return a * b;
+    if(a === '0' || b === '0'){
+        return '0';
+    }else
+        return Number(a) * Number(b);
 }
 
 let divide = function (a,b){
-    return a/b;
+    if(a === '0'){
+        return '0';
+    }else if(b === '0'){
+        return 'MATH ERROR'
+    }
+    return Number(a)/ Number(b);
 }
 
 let percentage = function (a){
-    return a/100;
+    return Number(a)/100;
+}
+
+let clear = function(){
+    num1 = undefined, num2 = undefined;
+    operand = '', operand1 = '';
+    ope = undefined, result = undefined;
+    bigF.textContent = '';
 }
 
 let operate = function (a,ope,b){
-    if(!(b)){
-        return percentage(a);
-    }else if(ope === '/'){
+    if(ope === '/'){
         return divide(a,b);
     }else if(ope === '+'){
         return add(a,b);
@@ -63,12 +76,13 @@ let operate = function (a,ope,b){
 
 const bigF = document.querySelector('.bigFont');
 const smallF = document.querySelector('.smallFont');
-let numbers = document.querySelectorAll('.num');
-let operators = document.querySelectorAll('.operate');
+const numbers = document.querySelectorAll('.num');
+const operators = document.querySelectorAll('.operate');
 
 let operand = '';
 let operand1 = '';
 let num1, ope, num2, result;
+// console.log(`${operand} ${operand1} ${num1} ${num2} ${ope} ${result}`);
 
 let getOperand = function(operand, text){
     if(!(text === '.')){
@@ -79,22 +93,30 @@ let getOperand = function(operand, text){
              operand += text;    
          }
      }
-     if(operand.length > 17) {
-        return operand.substring(0, 17);
+     if(operand.length > 12) {
+        return operand.substring(0, 12);
     }else{
         return operand
     }
 }
 
 let manipulating = function(nm1, oper, nm2, manipulator){
-    if(nm1 && oper && nm2){
+    if(nm1  && oper && nm2){
         result = operate(nm1, oper, nm2);
-        // smallF.textContent = `${num1} ${ope} ${num2}`;
-        num1 = result, operand1 = '', ope = manipulator;
-        bigF.textContent = `${result} ${ope}`;
+        if(!(result === 'MATH ERROR')){
+            num1 = Number(result);
+        }else if(result === 'MATH ERROR'){
+            num1 = undefined, num2 = undefined;
+            operand = '', operand1 = '';
+            ope = undefined, result = undefined;
+
+        }
+        
+        operand1 = '', ope = manipulator;
+        bigF.textContent = `${result}`;
     }else if(nm1 && !(oper) && !(nm2)){
         ope = manipulator;
-        bigF.textContent = `${num1} ${ope}`;
+        bigF.textContent = `${num1} `;
     }
 }
 
@@ -103,12 +125,12 @@ let input = function(number){
         let text = number.id;
          if(!(num1 && ope)){
             operand = getOperand(operand, text);
-             num1 = Number(operand);
+             num1 = operand;
              bigF.textContent = num1;
          }else if(num1 && ope){
              operand1 = getOperand(operand1, text);
-             num2 = Number(operand1);
-             bigF.textContent = `${num1} ${ope} ${num2}`;
+             num2 = operand1;
+             bigF.textContent = `${num2}`;
          }
     });
 }
@@ -132,6 +154,27 @@ let manipulate = function(operator){
                 result = percentage(n1);
                 num1 = result;
                 bigF.textContent = result;
+            }
+        }else if(manipulator === '='){
+            if(num1 && num2){
+                result = operate(num1, ope, num2);
+                num1 = result, num2 = undefined, ope = undefined, operand1 = '';
+                bigF.textContent = result;
+            }if(num1 && !(num2)){
+                result = num1;
+                bigF.textContent = result;
+            }
+        }else if(manipulator === 'clear'){
+            clear();
+        }else{
+            if(num1 && !(num2)){
+                operand = operand.slice(0, -1);
+                num1 = operand;
+                bigF.textContent = num1;
+            }else if(num1 && num2){
+                operand1 = operand1.slice(0, -1);
+                num2 = operand1;
+                bigF.textContent = num2;
             }
         }
 
